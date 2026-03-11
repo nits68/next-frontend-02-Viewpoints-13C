92bed2c30c7c73ad6b25922582a116921d7b6d1f
nits68 <nits.laszlo@jedlik.eu>
2026-03-11 13:42:09 +0100

    óra vége - finish

--- app/pagination/page.tsx
+++ app/pagination/page.tsx
@@ -1,52 +1,118 @@
+"use client";
+
+import axios from "axios";
 import { Search } from "lucide-react";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useGlobalStore } from "@/store/globalStore";
+
+export type ViewpointsItems = {
+  id: number;
+  viewpointName: string;
+  mountain: string;
+  locationId: number;
+  height: number;
+  description: string;
+  built: string;
+  imageUrl: string;
+  location: Location;
+};
+
+export type Location = {
+  id: number;
+  locationName: string;
+};
 
 export default function PaginationExamplePage() {
+  const [viewpoints, setViewpoints] = useState<ViewpointsItems[]>([]);
+  const { gs, set } = useGlobalStore();
+  const limit: number = 4;
+
+  useEffect(() => {
+    async function getViewpoints() {
+      const res = await axios.get(
+        `http://localhost:3000/api/viewpoints/${gs.actualPage}/${limit}/${gs.searchTerm || "*"}`,
+      );
+      setViewpoints(res.data);
+      set("numberOfRecords", res.headers["number-of-records"]);
+      set("numberOfPages", Math.ceil(gs.numberOfRecords / limit) || 1);
+      set("actualPage", gs.numberOfPages < gs.actualPage ? gs.numberOfPages : gs.actualPage);
+    }
+    getViewpoints();
+  }, [set, gs.numberOfRecords, gs.searchTerm, gs.actualPage, gs.numberOfPages]);
   return (
     <div className="justify-top flex flex-col items-center">
       {/* Input serch term */}
       <div className="m-3">
         <label className="input">
           <Search className="input-icon" />
-          <input placeholder="Keresés..." required type="search" />
+          <input
+            placeholder="Keresés..."
+            required
+            type="search"
+            value={gs.searchTerm}
+            onChange={(e) => set("searchTerm", e.target.value)}
+          />
         </label>
       </div>
       {/* First, previous, next, last buttons */}
       <div className="flex space-x-2">
-        <button className="btn btn-primary">First</button>
-        <button className="btn btn-primary">Previous</button>
-        <button className="btn btn-primary">Next</button>
-        <button className="btn btn-primary">Last</button>
+        <button
+          className="btn btn-primary"
+          disabled={gs.actualPage === 1}
+          onClick={() => set("actualPage", 1)}
+        >
+          First
+        </button>
+        <button
+          className="btn btn-primary"
+          disabled={gs.actualPage === 1}
+          onClick={() => set("actualPage", gs.actualPage - 1)}
+        >
+          Previous
+        </button>
+        <button
+          className="btn btn-primary"
+          disabled={gs.numberOfPages === gs.actualPage}
+          onClick={() => set("actualPage", gs.actualPage + 1)}
+        >
+          Next
+        </button>
+        <button
+          className="btn btn-primary"
+          disabled={gs.numberOfPages === gs.actualPage}
+          onClick={() => set("actualPage", gs.numberOfPages)}
+        >
+          Last
+        </button>
       </div>
       <div className="grid grid-cols-1 gap-10 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {/* Kártya példa */}
-        <div className="overflow-hidden rounded-xl bg-blue-200 shadow-md">
-          {/* Title bar */}
-          <div className="bg-blue-200 px-3 py-2 text-center font-semibold">
-            Csóványosi kilátó - (22.7 m) - Csóványos
-          </div>
+        {viewpoints.map((wp) => (
+          <div className="overflow-hidden rounded-xl bg-blue-200 shadow-md" key={wp.id}>
+            {/* Title bar */}
+            <div className="bg-blue-200 px-3 py-2 text-center font-semibold">
+              {wp.viewpointName} - ({wp.height} m) - {wp.mountain}
+            </div>
 
-          <div className="flex items-center justify-center">
-            <Image
-              alt="Csóványosi kilátó"
-              className="w-50 rounded-lg"
-              height={200}
-              src="https://nits68.github.io/static/viewpoints/csovanyos.jpg"
-              width={100}
-            />
-          </div>
+            <div className="flex items-center justify-center">
+              <Image
+                alt={wp.viewpointName}
+                className="w-50 rounded-lg"
+                height={200}
+                src={wp.imageUrl}
+                width={100}
+              />
+            </div>
 
-          <div className="m-4 rounded-md bg-yellow-100 p-3">
-            85 millió forintos költséggel, európai uniós támogatással épült, melynek során a tornyot
-            minden szinten körülsétálható, vörösfenyővel borított acélszerkezettel vették körül, és
-            a torony belsejében biztonságos, 133 fokos csigalépcsővel látták el.
-          </div>
+            <div className="m-4 rounded-md bg-yellow-100 p-3">{wp.description}</div>
 
-          <div className="mx-4 mb-4 rounded-md bg-blue-200 p-2 text-sm font-medium">
-            Hegység: Bükk, épült: 1939.05.14
+            <div className="mx-4 mb-4 rounded-md bg-blue-200 p-2 text-sm font-medium">
+              Hegység: {wp.location.locationName}, épült: {wp.built}
+            </div>
           </div>
-        </div>
+        ))}
       </div>
+      {/* {JSON.stringify(viewpoints)} */}
     </div>
   );
 }
